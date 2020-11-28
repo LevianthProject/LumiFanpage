bae2977fb869b54421874451b5df052abd75095b
Naufal Hakim <muhaqilanaufalhakim@gmail.com>
2020-11-28 09:11:05 +0700

    add popup

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ modals.push({
   icon: "success",
 });
 
+modals.push({
+    html: "<img src='https://media.discordapp.net/attachments/781371112755953689/781702082196537344/Lumi-Christmas-PosterV5.png?width=1249&height=702'>",
+    showCloseButton: true,
+    showCancelButton: false,
+    showConfirmButton: false,
+    grow: 'fullscreen',
+    background: 'transparent',
+});
+
 swal.queue(modals);
